89304e6acdd48f65cb17ece8dbed050cd79f83e5
Elgin <elgin_mclaren@hotmail.com>
2017-09-17 09:08:03 -0700

    added responsive views

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+//smooth scroll
+ 
+$(function() {
+  $('a[href*=\\#]:not([href=\\#])').on('click', function() {
+   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+    if (target.length) {
+     $('html,body').animate({
+      scrollTop: target.offset().top
+    }, 700);
+
+     return false;
+   }
+ }
+});
+});
